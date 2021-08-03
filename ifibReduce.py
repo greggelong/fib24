35ad5dd8edc3f1bb0896dd847a9990eabbd3fb05
Greg K <gkreisman_cep@qq.com>
2021-08-03 20:46:41 +0800

    fibb reduce

---
+++ ifibReduce.py
@@ -0,0 +1,33 @@
+def ifib(n):
+    '''iterative fibonacci'''
+    a = 0
+    b = 1
+    print(a)
+    print(b)
+    for i in range(2,n+1):
+        f = a+b
+        fr = reduce(f)
+        print(i,f, fr)
+        a,b = b,f
+
+
+def reduce(n):
+    '''use list comprehension '''
+    nlist = [int(x) for x in str(n)]
+    #print(nlist, len(nlist))
+    count =0
+    while len(nlist) > 1:
+        count +=1
+        sumit = sum(nlist)
+        #print(sumit)
+        nlist = [int(x) for x in str(sumit)]
+        #print(nlist, len(nlist))
+    return sum(nlist), count # returns the sum of a single list item converting it to int
+
+
+
+## prints number, fibonacci and (fibonacci reduction and number of times reduced)
+ifib(500)   
+
+
+ 
